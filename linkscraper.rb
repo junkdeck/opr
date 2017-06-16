2dbb0b4e6409c778e397773939ea9b47d9b31e34
junkdeck <fred@mslsr.com>
2017-06-16 16:27:56 +0200

    linkscraper: regex extracts multiple uri from singleline. useful for minified html

--- linkscraper.rb
+++ linkscraper.rb
@@ -1,4 +1,5 @@
 # some refactoring required. right now minified html does not scrape properly, as it's all on one line, so only the very first link gets selected.
+# FIXED, USES REGEX SCANNING TO FETCH MULTIPLE LINKS ON SINGLE LINE
 # i could just skip the whole "by-the-lines" process and directly RegExp#scan for any matches on /<a href=('|").+?('|")/
 # and store the matches in an array, then iterate through the array instead of using THREE(3) arrays!!!
 # might also rewrite the code to use classes and functions instead of one long procedural process that restarts by going back to the top - maybe do recursion instead?
@@ -22,21 +23,22 @@ while true
 
 	# error handling
 	begin
-	url = URI.parse(url_input)
-	response = Net::HTTP.start(url.host, url.port) do |http|
-		http.get(url.path)
-	end
+		url = URI.parse(url_input)
+		response = Net::HTTP.start(url.host, url.port) do |http|
+			http.get(url.path)
+		end
 	rescue
 		puts "Couldn't establish a connection, try again!"
 		# goes to the next iteration of the while loop, ignoring rest of code
-	 	next
+		next
 	end
 	# end of error handling
 
 	content = response.body
 	# returns the subgroup containing the actual path on every match on the regular expression.
-	# looks for anchor tags and accepts both quotes and apostrophes. ignores empty hashtag links.
-	links << content.scan(/<a href=['|"]([^#].+?)['|"]/)
+	# looks for anchor tags and accepts both quotes and apostrophes. ignores empty and reference hashtag links
+	link_pattern = /<a href=['"]([^#]+?)['"]/
+	links << content.scan(link_pattern)
 	puts links
 
 	# content.each_line do |line|
@@ -44,26 +46,26 @@ while true
 	# 		lines_including_links << line.strip
 	# 	end
 	# end
-		# lines_including_links.each do |line|
-		# 	rx = /href=('|").+?('|")/
-		# 	new = rx.match(line).to_s
-		# 	url_only = /('|").+?('|")/.match(new)
-		# 	links << url_only.to_s[1..-2]
-		# end
-		#
-		# links.each do |x|
-		# 	if x.class != NilClass
-		# 		if x[0] == '.'
-		# 			links_absolute << "http://#{url.host}/#{x[2..-1]}"
-		# 		elsif x[0] == '/'
-		# 			links_absolute << "http://#{url.host}/#{x[1..-1]}"
-		# 		else
-		# 			links_absolute << x
-		# 		end
-		# 	end
-		# end
-		# links_absolute[0] = "No links found!" if links.empty?
-		# puts links_absolute.uniq.sort
-		# puts links.empty? ? "No links found!" : links_absolute.uniq.sort
-		# links_absolute = []
-	end
+	# lines_including_links.each do |line|
+	# 	rx = /href=('|").+?('|")/
+	# 	new = rx.match(line).to_s
+	# 	url_only = /('|").+?('|")/.match(new)
+	# 	links << url_only.to_s[1..-2]
+	# end
+	#
+	# links.each do |x|
+	# 	if x.class != NilClass
+	# 		if x[0] == '.'
+	# 			links_absolute << "http://#{url.host}/#{x[2..-1]}"
+	# 		elsif x[0] == '/'
+	# 			links_absolute << "http://#{url.host}/#{x[1..-1]}"
+	# 		else
+	# 			links_absolute << x
+	# 		end
+	# 	end
+	# end
+	# links_absolute[0] = "No links found!" if links.empty?
+	# puts links_absolute.uniq.sort
+	# puts links.empty? ? "No links found!" : links_absolute.uniq.sort
+	# links_absolute = []
+end
