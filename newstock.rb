a72c377bf2f42d9437363a94c36e988be2d0889b
junkdeck <fred@mslsr.com>
2017-06-18 20:00:01 +0200

    stockpicker: cross-breed algo

--- newstock.rb
+++ newstock.rb
@@ -1,5 +1,5 @@
 def stock_picker(days)
-  # stock picker - buy low, sell high - only sell after buying
+  # stock picker - buy low, sell high
   low = days.min
   high = days.max
 
@@ -12,6 +12,6 @@ def stock_picker(days)
   sell_day = days.find_index(high)
   return [buy_day, sell_day, low, high]
 end
-days = [17,3,4,9,15,8,6,2,1]
+days = [17,3,15,9,15,1,8,6,2,1]
 buy, sell, low, high = stock_picker(days)
 puts "Buy on Day #{buy} for #{low} units.\nSell on Day #{sell} for #{high} units."
--- stock.rb
+++ stock.rb
@@ -1,21 +1,26 @@
 def stock_picker(days)
   high, low = [0, days[0]]
-  buy, sell = [0,0]
   has_bought = false
 
   days.each_with_index do |x,i|
     if x < low
       low = x
-      buy = i
       has_bought = true if low == days.min
     end
     if x > high && has_bought == true
       high = x
-      sell = i
     end
   end
-  return [buy, sell, low, high]
+  if days[0] == high || days[-1] == low
+    days.shift if days[0] == high
+    days.pop if days[-1] == low
+    buy_day, sell_day, low, high = stock_picker(days)
+  end
+  buy_day = days.find_index(low)
+  sell_day = days.find_index(high)
+  return [buy_day, sell_day, low, high]
+  # return [buy, sell, low, high]
 end
-days = [17,3,4,9,15,8,6,2,1]
+days = [17,3,15,4,9,15,1,8,6,2,1]
 buy, sell, low, high = stock_picker(days)
 puts "Buy on Day #{buy} for #{low} units\nSell on Day #{sell} for #{high} units"
