cea20553a7da0bf9831e3665e9d1cb2cbaef0234
junkdeck <fred@mslsr.com>
2017-06-21 00:42:41 +0200

    xo: winning logic, new game

--- xo.rb
+++ xo.rb
@@ -1,13 +1,13 @@
 
 # cli tic tac toe, oop style
 class Game
-  attr_reader :board
   @@game_running = true
 
   def initialize
-    @current_player = 0
+    @current_player = rand(0..1)
     @board = Board.new
     @tagline = tagline_generator
+    @won = false
   end
 
   def tagline_generator
@@ -16,9 +16,8 @@ class Game
       "family style",
       "oldschool",
       "roguelite puzzler",
-      "desperate scramble for a portfolio",
+      "desperate scramble for a portfolio piece",
       "Y2K compliant",
-      "over the shoulder boulder holder",
       "never-seen-before",
       "illustrious career all started with",
       "diet contains natural sources of",
@@ -41,12 +40,12 @@ class Game
       "early access",
       "crowdfunded",
       "carrot-top of cli entertainment",
-      "enterprise-level server infrastructure",
+      "enterprise-level server infrastructure framework",
       "Brand New",
-      "Europe's Finest",
       "dynamic tagline generated",
-      "word of the wise: do not operate heavy machinery after using",
-      "ruby implementation of tic-tac-toe"
+      "personal warning: do not operate heavy machinery after using",
+      "ruby implementation of tic-tac-toe",
+      "journey to the planet named",
     ]
     tag = rand(0..tags.length+1)
     return tags[tag]
@@ -64,7 +63,7 @@ class Game
     ██╔╝ ██╗╚██████╔╝      ██║  ██║╚██████╔╝██████╔╝   ██║
     ╚═╝  ╚═╝ ╚═════╝       ╚═╝  ╚═╝ ╚═════╝ ╚═════╝    ╚═╝
     ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─ ─
-    supply coordinates as XY, where X is A-C and Y is 1-3
+    enter coordinates in this format : letter, number
     "
     puts message
   end
@@ -72,23 +71,28 @@ class Game
   def main_loop
     while @@game_running
       hello_message # displays the fancy logo and instructions on how to play
-      # updates the game board to reflect changes brought by inputting coordinates
-      puts "\tPlayer #{@current_player + 1}, you're up!"
+
+      puts "Player #{@current_player + 1}, you're up!" unless @won
       if @error_message
         puts "\t#{@error_message}"
         @error_message = nil
       else
         print "\n"
       end
-      @board.draw_board
 
+      @board.draw_board
       # error handling block
       begin
         input = get_input
         print "\n"  # lil linebreak
 
-        fill_square(input)
-        @board.check_for_winner
+        if @won
+          new_game
+          next
+        else
+          fill_square(input)
+        end
+
       rescue ArgumentError
         @error_message = "Hey! That square's already occupied!"
       rescue IOError
@@ -99,11 +103,23 @@ class Game
         exit
       else
         # switches between player 1 and 2
-        toggle_player
+        if check_for_winner
+          @error_message = "Player #{@current_player + 1} wins!"
+          @won = true
+        else
+          toggle_player
+        end
         system("clear")
         # nothing left to do! next round, please!
       end
     end
+  end
+
+  def new_game
+    # sets the current player, assigns a board instance variable, a random tagline and resets the win state
+    @current_player ^= 1
+    @board = Board.new
+    @won = false
   end
 
   def get_input
@@ -112,7 +128,7 @@ class Game
     coords = input.scan(/^([a-cA-C][1-3])/).join('')
 
     unless coords.length > 0
-      raise IOError
+      raise IOError unless @won
     end
     return coords
   end
@@ -122,8 +138,32 @@ class Game
     @current_player ^= 1
   end
 
+  def check_for_winner
+    winner = false
+    winning_combo = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+    ]
+
+    ["X","O"].each do |mark|
+      winning_combo.each do |x,y,z|
+        if @board.squares[x] == mark && @board.squares[y] == mark && @board.squares[z] == mark
+          # @@game_running = false
+          winner = @current_player
+        end
+      end
+    end
+    return winner
+  end
+
   def fill_square(input)
-    case @current_player
+    case @current_player    # determines what mark to use
     when 0
       mark = 'X'
     when 1
@@ -131,7 +171,6 @@ class Game
     else
       mark = '$'
     end
-
     # finds the square corresponding to the user's input coordinates.
     # input downcased and symbolized for case insensitivity
     square_index = @board.coords[input.to_sym]
@@ -173,25 +212,7 @@ class Game
       "\t  ╠═══╬═══╬═══╣\n"\
       "\t3 ║ #{@squares[6]} ║ #{@squares[7]} ║ #{@squares[8]} ║\n"\
       "\t  ╚═══╩═══╩═══╝\n\n\n"
-      puts message.rjust(40)
-    end
-
-    def check_for_winner
-      winning_combo = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,7],
-        [2,4,6]
-      ]
-
-      @squares.each do |x|
-        puts x
-      end
-
+      puts message
     end
   end
 
