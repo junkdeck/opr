d5ad77c318674d3223baad22d4bb1add28d2278e
junkdeck <fred@mslsr.com>
2017-06-22 17:13:23 +0200

    mastermind: persistent screen display, input and error handling

---
+++ mastermind.rb
@@ -0,0 +1,65 @@
+require_relative "tagline"
+
+class Game
+  # include Tagline
+
+  def initialize
+    @tag = Tagline::tagline_generator
+    @err_msg = ""
+  end
+
+  def main_loop
+    loop do
+      render_screen
+      print ">" # screen prompt
+      begin
+        input = get_input
+      rescue Interrupt
+        break_game
+      end
+    end
+  end
+
+  def get_input
+    input = gets.chomp.scan(/\d/).map(&:to_i) # strips out all non-letters
+    unless input.length == 4 && input.all?{|x| x.between?(0,6)}
+      # checks for bogus input and sets error message accordingly
+      @err_msg = "Input must be 4 digits between 0 and 6!"
+    end
+  end
+
+  def render_screen
+    # renders the changed game state to screen
+    system("clear")   # flush display before amending new data for a clean pro look
+
+    title = "
+    junkdeck's #{@tag}
+
+    • ▌ ▄ ·.  ▄▄▄· .▄▄ · ▄▄▄▄▄▄▄▄ .▄▄▄  • ▌ ▄ ·. ▪   ▐ ▄ ·▄▄▄▄
+    ·██ ▐███▪▐█ ▀█ ▐█ ▀. •██  ▀▄.▀·▀▄ █··██ ▐███▪██ •█▌▐███▪ ██
+    ▐█ ▌▐▌▐█·▄█▀▀█ ▄▀▀▀█▄ ▐█.▪▐▀▀▪▄▐▀▀▄ ▐█ ▌▐▌▐█·▐█·▐█▐▐▌▐█· ▐█▌
+    ██ ██▌▐█▌▐█ ▪▐▌▐█▄▪▐█ ▐█▌·▐█▄▄▌▐█•█▌██ ██▌▐█▌▐█▌██▐█▌██. ██
+    ▀▀  █▪▀▀▀ ▀  ▀  ▀▀▀▀  ▀▀▀  ▀▀▀ .▀  ▀▀▀  █▪▀▀▀▀▀▀▀▀ █▪▀▀▀▀▀•
+    = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+"
+  puts title
+  print "#{@err_msg}\n"
+  unless @err_msg.empty?
+    @err_msg = ""
+  end
+  end
+
+  def break_game
+    system('clear')
+    puts "Thanks for playing!"
+    exit
+  end
+
+  class Player
+  end
+  class Combination
+  end
+end
+
+mm = Game.new
+mm.main_loop
---
+++ tagline.rb
@@ -0,0 +1,42 @@
+module Tagline
+  def self.tagline_generator
+    tags = [
+      "",
+      "family style",
+      "vintage time waster",
+      "roguelite puzzler",
+      "desperate scramble for a portfolio piece",
+      "Y2K compliant",
+      "never-seen-before",
+      "illustrious career all started with",
+      "diet contains natural sources of",
+      "chopped & screwed",
+      "proud to present",
+      "sick and tired of",
+      "original recipe",
+      "visually impressive",
+      "dangerously spicy",
+      "hot and heavy",
+      "interdimensional nosedive",
+      "curio of the millenium",
+      "straight out the oven",
+      "sparkly and clean",
+      "drinking that sweet, sweet",
+      "homemade",
+      "jerry-rigged",
+      "barely functional",
+      "multi-player only",
+      "early access",
+      "crowdfunded",
+      "carrot-top of cli entertainment",
+      "enterprise-level server infrastructure framework",
+      "Brand New",
+      "dynamic tagline generated",
+      "personal warning: do not operate heavy machinery after using",
+      "ruby implementation of tic-tac-toe",
+      "journey to the planet named",
+    ]
+    tag = rand(0..tags.length+1)
+    return tags[tag]
+  end
+end
