1e138005fe0441755c0743784ac8d0ffe440ab0d
junkdeck <fred@mslsr.com>
2017-06-22 23:13:33 +0200

    mm: code gen, history store and display, feedback on precision of guess

--- mastermind.rb
+++ mastermind.rb
@@ -6,6 +6,9 @@ class Game
   def initialize
     @tag = Tagline::tagline_generator
     @err_msg = ""
+    @combo = Combination.new
+    @remaining_turns = 12
+    @history = Hash.new(0)
   end
 
   def main_loop
@@ -15,18 +18,22 @@ class Game
       begin
         input = get_input
       rescue ArgumentError
-        @err_msg = "Input must be 4 digits between 0 and 6!"
+        @err_msg = "Input must be 4 digits between 1 and 6!"
       rescue Interrupt
         break_game
+      else
+        @history[@history.length] = {guess: input, feedback: @combo.try_guess(input)}
       end
     end
   end
 
   def get_input
-    input = gets.chomp.scan(/\d/).map(&:to_i) # strips out all non-letters
-    unless input.length == 4 && input.all?{|x| x.between?(0,6)}
+    input = gets.strip.chomp.scan(/\d/).map(&:to_i) # strips out all non-letters
+    unless input.length == 4 && input.all?{|x| x.between?(1,6)}
       # checks for bogus input and sets error message accordingly
       raise ArgumentError
+    else
+      return input
     end
   end
 
@@ -34,7 +41,7 @@ class Game
     # renders the changed game state to screen
     system("clear")   # flush display before amending new data for a clean pro look
 
-    title = "
+    puts "
     junkdeck's #{@tag}
 
     • ▌ ▄ ·.  ▄▄▄· .▄▄ · ▄▄▄▄▄▄▄▄ .▄▄▄  • ▌ ▄ ·. ▪   ▐ ▄ ·▄▄▄▄
@@ -43,13 +50,29 @@ class Game
     ██ ██▌▐█▌▐█ ▪▐▌▐█▄▪▐█ ▐█▌·▐█▄▄▌▐█•█▌██ ██▌▐█▌▐█▌██▐█▌██. ██
     ▀▀  █▪▀▀▀ ▀  ▀  ▀▀▀▀  ▀▀▀  ▀▀▀ .▀  ▀▀▀  █▪▀▀▀▀▀▀▀▀ █▪▀▀▀▀▀•
     = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-        guess the code! input accepts 4 digits between 0-6
-"
-  puts title
-  print "\t#{@err_msg}\n"
-  unless @err_msg.empty?
-    @err_msg = ""
+    guess the code! input accepts 4 digits between 1-6"
+    print "\t#{@err_msg}\n"
+    unless @err_msg.empty?
+      @err_msg = ""
+    end
+
+    # puts "HISTORY: #{@history}"
+
+    @history.each do |index,log|
+      puts "#{log[:guess]}, #{log[:feedback]}"
+      # puts "#{x[:guess]} | #{x[:feedback]}"
+    end
+    # board = "#{@combo.code[0]} #{@combo.code[1]} #{@combo.code[2]} #{@combo.code[3]} "\
+    # "| #{@combo.feedback[0]} #{@combo.feedback[1]} #{@combo.feedback[2]} #{@combo.feedback[3]}" if @history
+    # user guess > feedback
+    # puts board
+
   end
+
+  def new_game
+    @combo = Combination.new    # generate new code
+    @remaining_turns = 12       # reset amount of turns
+    @history = Hash.new(0)      # reset guess history
   end
 
   def break_game
@@ -61,6 +84,32 @@ class Game
   class Player
   end
   class Combination
+    attr_reader :code, :feedback
+
+    def initialize
+      # if player chooses
+      # combination = Game::get_input
+      @code = []
+      @feedback = []
+      4.times do
+        @code << rand(1..6)
+      end
+    end
+
+    def try_guess(guess)
+      @feedback = []    # empty feedback from previous guess
+      # compares the guess with the randomly generated code - o is correct number, O is correct number & position
+      guess.each_with_index do |x,i|
+        if @code.include?(x)
+          if @code[i] == x
+            @feedback << 'O'
+          else
+            @feedback << 'o'
+          end
+        end
+      end
+      return @feedback
+    end
   end
 end
 
